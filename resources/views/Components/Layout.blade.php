db24f55bd57c223fa9b1eae4ab7e670233a5128f
Delanoth <150026391+Delanoth@users.noreply.github.com>
2024-09-23 10:38:41 +0200

    les 5

--- resources/views/Components/Layout.blade.php
+++ resources/views/Components/Layout.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full bg-gray-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>home page</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+<body class="h-full">
+
+
 <!--
   This example requires updating your template:
 
@@ -25,8 +27,8 @@
             </div>
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
-                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
+                
+                <a href="/" class=" {{ true ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white'}}rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
                 <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
                 <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
               </div>
